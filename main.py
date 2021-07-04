45b5e3382b8df7fd1ad4e719f215c211cc229694
Lefia <lefia1222@gmail.com>
2021-07-04 10:54:31 +0800

    Add book title into json

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ Books = data.open()
 content = Content("")
 
 for i in range(len(Books["Id"])): 
-    lastest, book_title, title = Crawler.info(Books["Id"][i])
+    lastest, title = Crawler.info(Books["Id"][i])
     if Books["Lastest"][i] != lastest: #如果文章更新了
-        content.add(Books["Id"][i], lastest, book_title, title)
+        content.add(Books["Id"][i], lastest, Books["BookTitle"][i], title)
         Books["Lastest"][i] = lastest #更新最新小說id
 
 content.exists()
--- mod/crawler.py
+++ mod/crawler.py
@@ -11,6 +11,5 @@ class Crawler:
         a = link[-1]
         lastest = a['href'][30+len(book):-5]
         title = a.find('p').get_text()
-        book_title = soup.find(class_="p-t-10").get_text()
-        return lastest, book_title, title
-        
+        # book_title = soup.find(class_="p-t-10").get_text()
+        return lastest, title
