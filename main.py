5b86f24d8f0359193c38bd5c7aeeb140253c9b60
Lefia <lefia1222@gmail.com>
2021-07-11 21:32:55 +0800

    add new book

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ content = Content("")
 
 for i in range(len(Books["Id"])): 
     lastest, title = Crawler.info(Books["Id"][i])
-    if Books["Lastest"][i] != lastest: #如果文章更新了
+    if Books["Lastest"][i] != lastest: 
         content.add(Books["Id"][i], lastest, Books["BookTitle"][i], title)
-        Books["Lastest"][i] = lastest #更新最新小說id
+        Books["Lastest"][i] = lastest 
 
 content.exists()
 
