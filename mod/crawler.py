15d9c575cf5608f2c9e54431484d6d6f40b69385
Lefia <lefia1222@gmail.com>
2021-07-24 23:08:51 +0800

    add header

--- mod/crawler.py
+++ mod/crawler.py
@@ -4,8 +4,9 @@ from bs4 import BeautifulSoup
 class Crawler:
 
     def info(book, wantBookTitle=False):
+        headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36 Edg/92.0.902.55'}
         url = f"https://www.esjzone.cc/detail/{ book }.html"
-        htmlFile = requests.get(url)
+        htmlFile = requests.get(url, headers=headers)
         soup = BeautifulSoup(htmlFile.text, 'lxml')
         chapterlist = soup.find("div", attrs={"id":"chapterList"})
         link_div = chapterlist.find_all("a")
